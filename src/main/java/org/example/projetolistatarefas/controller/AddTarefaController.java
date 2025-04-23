ed15708931c1c76f27516abbce6509fc6524b38d
Bruno <bteles400@gmail.com>
2025-04-23 16:28:49 -0300

    Segundo commit

--- src/main/java/org/example/projetolistatarefas/controller/AddTarefaController.java
+++ src/main/java/org/example/projetolistatarefas/controller/AddTarefaController.java
@@ -1,4 +1,105 @@
 package org.example.projetolistatarefas.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import org.example.projetolistatarefas.Dao.ConnectionFactory;
+import org.example.projetolistatarefas.Dao.TarefaDao;
+import org.example.projetolistatarefas.Model.Tarefa;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
 public class AddTarefaController {
+    @FXML private TextField tituloTxt;
+    @FXML private TextArea descTxt;
+    @FXML private ComboBox<String> statusCbox;
+    @FXML private DatePicker dataPick;
+    @FXML private Button salvarButton;
+    @FXML private Button voltarButton;
+
+    private TarefaDao tarefaDao;
+
+    @FXML
+    public void initialize(){
+        Connection connection = ConnectionFactory.getConnection();
+        tarefaDao = new TarefaDao(connection);
+        statusCbox.getItems().addAll("A fazer", "Em andamento", "Concluída");
+
+        salvarButton.setOnAction(actionEvent -> {
+            try {
+                salvarTarefa();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        voltarButton.setOnAction(actionEvent -> {
+            try {
+                voltarLista();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private void salvarTarefa() throws SQLException {
+        String titulo = tituloTxt.getText();
+        String descricao = descTxt.getText();
+        LocalDate localDate = dataPick.getValue();
+
+        java.sql.Date dataEntrega = null;
+        if (localDate != null) {
+            dataEntrega = java.sql.Date.valueOf(localDate);
+        }
+        String status = statusCbox.getValue();
+
+        if(titulo.isEmpty() || descricao.isEmpty() || dataEntrega == null || status.isEmpty()){
+            campoVazio();
+        } else {
+            Tarefa tarefa = new Tarefa(titulo, descricao, dataEntrega, status);
+            tarefaDao.adicionarTarefa(tarefa);
+
+            tituloTxt.clear();
+            descTxt.clear();
+            dataPick.setValue(null);
+            statusCbox.setValue(null);
+
+            tarefaAdicionada();
+        }
+
+    }
+
+    private void campoVazio(){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Erro");
+        alert.setHeaderText(null);
+        alert.setContentText("Preencha todos os campos");
+        alert.showAndWait();
+    }
+
+    private void tarefaAdicionada(){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Parabéns");
+        alert.setHeaderText(null);
+        alert.setContentText("Cadastro realizado com sucesso!");
+        alert.showAndWait();
+    }
+
+    private void voltarLista() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/projetolistatarefas/lista-tarefas.fxml"));
+        Parent root= loader.load();
+
+        Stage stage= new Stage();
+        stage.setScene(new Scene(root));
+        stage.show();
+
+        Stage atualStage = (Stage) voltarButton.getScene().getWindow();
+        atualStage.close();
+    }
+
 }
--- src/main/java/org/example/projetolistatarefas/controller/ListaTarefasController.java
+++ src/main/java/org/example/projetolistatarefas/controller/ListaTarefasController.java
@@ -1,10 +1,16 @@
 package org.example.projetolistatarefas.controller;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import org.example.projetolistatarefas.Dao.ConnectionFactory;
 import org.example.projetolistatarefas.Dao.TarefaDao;
 import org.example.projetolistatarefas.Model.Tarefa;
@@ -12,12 +18,23 @@ import org.example.projetolistatarefas.Model.Tarefa;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
 
 public class ListaTarefasController {
     @FXML private VBox tarefasBox;
     @FXML private Label dataTela;
     @FXML private Label horaTela;
+    @FXML private Button addButton;
+
+    LocalDate dataHoje = LocalDate.now();
+
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
     private TarefaDao tarefaDao;
 
@@ -25,7 +42,19 @@ public class ListaTarefasController {
     public void initialize() throws SQLException {
         Connection connection = ConnectionFactory.getConnection();
         tarefaDao = new TarefaDao(connection);
+
         carregarTarefas();
+        atualizarHora();
+        dataTela.setText(String.valueOf(dataHoje));
+
+        addButton.setOnAction(actionEvent -> {
+            try {
+                telaAddTarefa();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
     }
 
     private void carregarTarefas() throws SQLException {
@@ -45,5 +74,36 @@ public class ListaTarefasController {
                 e.printStackTrace();
             }
         }
+    }
+
+    private void atualizarHora(){
+        Thread thread = new Thread(() -> {
+           while (true) {
+               LocalTime agora = LocalTime.now();
+               String horaFormatada = agora.format(formatter);
+
+               Platform.runLater(() -> horaTela.setText(horaFormatada));
+
+               try{
+                   Thread.sleep(1000);
+               } catch (InterruptedException e){
+                   e.printStackTrace();
+               }
+           }
+        });
+        thread.setDaemon(true);
+        thread.start();
+    }
+
+    private void telaAddTarefa() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/projetolistatarefas/add-tarefa.fxml"));
+        Parent root= loader.load();
+
+        Stage stage= new Stage();
+        stage.setScene(new Scene(root));
+        stage.show();
+
+        Stage atualStage = (Stage) addButton.getScene().getWindow();
+        atualStage.close();
     }
 }
